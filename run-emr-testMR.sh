dfd70151378c1c20e66f97a1035bb094243e9eb4
Sujee Maniyam <sujee@sujee.net>
2011-01-29 23:57:33 -0800

    fix for launch script

--- run-emr-testMR.sh
+++ run-emr-testMR.sh
@@ -18,13 +18,5 @@ export t1=$(date +%s)
 #if the line breaks don't work, join the following lines and remove all '\'
 export JOBID=$(elastic-mapreduce --plain-output  --create --name "${JOBNAME}__${TIMESTAMP}"   --num-instances "$INSTANCES"  --master-instance-type "$MASTER_INSTANCE_TYPE"  --slave-instance-type "$SLAVE_INSTANCE_TYPE"  --jar s3://my_bucket/my.jar --main-class my.TestMR  --log-uri s3://my_bucket/emr-logs/ )
 
-export JOBID=$(elastic-mapreduce --plain-output  --create --name "${JOBNAME}__${TIMESTAMP}"\
---num-instances "$INSTANCES"  --master-instance-type "$MASTER_INSTANCE_TYPE"  --slave-instance-type "$SLAVE_INSTANCE_TYPE" \
---jar s3://my_bucket/my.jar \
---main-class my.TestMR --log-uri s3://my_bucket/emr_logs/  \
---bootstrap-action s3://elasticmapreduce/bootstrap-actions/configure-hadoop \
---args "--core-config-file,s3://my_bucket/config-core-site.xml,\
---mapred-config-file,s3://my_bucket/config-mapred-site-m1-xl.xml")
-
 
 sh ./emr-wait-for-completion.sh
